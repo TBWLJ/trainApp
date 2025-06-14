0a45a645acc07e2a9d394bed6c1073616d158985
TBWLJ <taiwoayomide202@gmail.com>
2025-06-14 23:26:43 +0100

    today commit

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import Header from './components/Header';
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-2xl font-bold text-blue-600">Tailwind is working! 🎉</Text>
+    <View style={styles.container}>
+      <View>
+        <Header/>
+      </View>
+      <Text style={styles.text}>Ayomide is coming! 🎉</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#ffffff', // Tailwind white
+    paddingTop: 50,
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#3b82f6', // Tailwind blue-600
+  },
+});
