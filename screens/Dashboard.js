91ed9e3fb6238347111a3b2cc3649fca3643e39d
TBWLJ <taiwoayomide202@gmail.com>
2025-06-30 18:48:25 +0100

    screens for dashboard

--- screens/Dashboard.js
+++ screens/Dashboard.js
@@ -1,14 +1,43 @@
-import React from "react"
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import DashboardHome from "../dashboard/DashboardHome"
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
+import DashboardHome from "../dashboard/DashboardHome";
+import Profile from "../dashboard/Profile";
+import Notifications from "../dashboard/Notification";
+import Settings from "../dashboard/Setting";
 
 const Tab = createBottomTabNavigator();
 
 export default function Dashboard() {
-    return (
-        <Tab.Navigator screenOptions={{ headerShown: false }}>
-            <Tab.Screen name="Home" component={DashboardHome} />
-        </Tab.Navigator>
-    );
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Profile") {
+            iconName = focused ? "person" : "person-outline";
+          } else if (route.name === "Notifications") {
+            iconName = focused ? "notifications" : "notifications-outline";
+          } else if (route.name === "Settings") {
+            iconName = focused ? "settings" : "settings-outline";
+          }
+
+          // Return the icon component
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: "tomato",
+        tabBarInactiveTintColor: "gray",
+      })}
+    >
+      <Tab.Screen name="Home" component={DashboardHome} />
+      <Tab.Screen name="Profile" component={Profile} />
+      <Tab.Screen name="Notifications" component={Notifications} />
+      <Tab.Screen name="Settings" component={Settings} />
+    </Tab.Navigator>
+  );
 }
