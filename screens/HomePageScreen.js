56abe1f17ac1e127f218129aebe80231dfac9518
TBWLJ <taiwoayomide202@gmail.com>
2025-06-30 01:42:21 +0100

    homescreen completed

--- screens/HomePageScreen.js
+++ screens/HomePageScreen.js
@@ -1,23 +1,52 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button, Image, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, Button, Image, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native'
 import cp from "../assets/cp.png"
 
 export default function HomePageScreen( navigation ) {
   return (
-    <View style={styles.container}>
-      <Text>Home Page</Text>
-      <View style={styles.inner}>
-        <View>
-          <Image source={cp} style={styles.image} />
+    <SafeAreaView style={styles.container}>
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View style={styles.inner}>
+          <View>
+            <Image source={cp} style={styles.image} />
+          </View>
+          <View>
+            <Button title="Login" onPress={() => navigation.navigate('Login')} />
+          </View>
         </View>
         <View>
-          <Button title="Login" onPress={() => navigation.navigate('Login')} />
+          <View style={styles.titleHead}>
+            <Text style={styles.title}>Simplify Your Freelance Projects with Branded Client Portals</Text>
+            <Text style={styles.paragText}>Manage project updates, share files, send invoices -- all in one beautiful space.</Text>
+            <View style={styles.navButtonHead}>
+              <TouchableOpacity
+              style={styles.navButton}
+            >
+              <Text style={styles.navText}>Get started for free</Text>
+            </TouchableOpacity>
+            </View>
+          </View>
         </View>
-      </View>
-      <View>
-        <Text></Text>
-      </View>
-    </View>
+        <View style={styles.bigHead}>
+          <View style={styles.bigHead}>
+            <Text style={styles.featureTitle}>Client Portals</Text>
+            <Text style={styles.feature}>Create private, shareable pages for each client. No login needed.</Text>
+          </View>
+          <View style={styles.bigHead}>
+            <Text style={styles.featureTitle}>File Sharing & Updates</Text>
+            <Text style={styles.feature}>Upload projetc files and keep clients informed with updates.</Text>
+          </View>
+          <View style={styles.bigHead}>
+            <Text style={styles.featureTitle}>Invoices & Payments</Text>
+            <Text style={styles.feature}>Send invoices and get paid quickly with Paystack/Flutterwave</Text>
+          </View>
+        </View>
+        {/* Footer */}
+        <View>
+          <Text style={styles.paragText}>2025 Panelly -- All rights reserverd</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
@@ -27,17 +56,58 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     marginTop: 25
   },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginButtom: 20,
-  },
   inner: {
-    display: 'flex',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    padding: 20
   },
   image: {
-    width: 20,
-    height: 20
+    width: 30,
+    height: 30
+  },
+  titleHead: {
+    display: "flex",
+    marginTop: 30,
+    justifyContent: "center"
+  },
+  title: {
+    fontSize: 40,
+    fontWeight: "bold",
+    textAlign: "center",
+    fontFamily: "Impact",
+    lineHeight: 50,
+    color: "#3DC2AA"
+  },
+  paragText: {
+    textAlign: "center"
+  },
+  navButtonHead: {
+    flexDirection: 'row',
+    justifyContent: "center",
+    alignItems: "center",
+    margin: 12
+  },
+  navButton: {
+    backgroundColor: "#3DC2AA",
+    paddingHorizontal: 20,
+    paddingVertical: 12,
+    borderRadius: 12
+  },
+  navText: {
+    color: "#fff",
+    fontWeight: "bold"
+  },
+  bigHead: {
+    paddingVertical: 16,
+    paddingHorizontal: 10
+  },
+  featureTitle: {
+    color: "#3DC2AA",
+    fontWeight: "bold",
+    fontSize: 20
+  },
+  feature: {
+    color: "#333",
+    fontSize: 16
   }
 })
